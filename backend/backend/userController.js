bfdab9357e3130a2fc37caa66aea40a27718087d
Hung <tanhungtranhh@gmail.com>
2025-10-16 23:02:29 +0700

    Add backend: server, routes, controllers, models

---
+++ backend/backend/userController.js
@@ -0,0 +1,22 @@
+const User = require("../models/User");
+
+// GET all users
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// POST create new user
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
