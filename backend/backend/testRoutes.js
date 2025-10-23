edf8427608bf6b6ee2eb2a45d0ec2e243131a129
Ho Quoc Cuong <Hoquocccuongg@gmail.com>
2025-10-23 22:51:40 +0700

    Kết nối MongoDB thành công - Hồ Quốc Cường Nhóm 5

--- backend/backend/testRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.json({ message: "✅ Backend is working perfectly!" });
-});
-
-module.exports = router;
